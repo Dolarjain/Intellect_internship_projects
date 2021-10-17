9715fecfd88ea8c6bd34d1721e1af5dfb23e3714
Dolarjain <78502607+Dolarjain@users.noreply.github.com>
2021-10-17 11:58:05 +0530

    Add files via upload
    multiple types of printing or formating texts

---
+++ practical1_assignment_on_printing.py
@@ -0,0 +1,45 @@
+#it will print massage hallo then it will go to next line and print world
+# means if we use print function it will take a whole line. 
+print("Hallo")
+print("World")
+
+#if we write only print function without parameter or value it will also take that whole line.
+print()
+
+#here end is for formating of when string will end thair execute \n which is for new line.
+print("Hello",end='\n')
+print()
+
+#if we write two string or parameter inside print function with the use of comma(,) it will get one spece by defualt 
+#when we use the end or sep formating parameter then it's not getting a space
+print("Hallo","good",end=' night')
+
+#how to print Spaces in python
+print(" ")
+print(' ') # we can also use single quates to print strings in print functions
+print("hallo world") # here we can easily put the space as per we write in our daily life
+print("hello    world") #here if we put multiple blank spaces one by one it will print as it is as in string
+
+#how to printing different values in python(int,float,string,boolean)
+print(12) # this is a declaretion of integer value
+print(12.5) #this is a float value declaration 
+print("dolar") # this is a string value declaration
+print(True) #in the boolean value declaration must to use first character capital
+
+# arthematical opration inside the print() function
+print(10 + 20)
+print(20.5 + 19.5)
+print("dolar"+"jain")
+print(True+True)
+
+#printing the diffrent types of variables 
+
+a=20               
+b=22.2             
+c="Python"
+d=True
+
+print(a)
+print(b)
+print(c)
+print(d)
