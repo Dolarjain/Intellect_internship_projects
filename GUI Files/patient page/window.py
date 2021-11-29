77f933443553eb961e2873c79595cee02f3d82a6
Dolar jain <78502607+Dolarjain@users.noreply.github.com>
2021-11-29 23:08:13 +0530

    Add files via upload

---
+++ GUI Files/patient page/window.py
@@ -0,0 +1,67 @@
+from tkinter import *
+
+
+def btn_clicked():
+    print("Button Clicked")
+
+
+window = Tk()
+
+window.geometry("1000x600")
+window.configure(bg = "#ffffff")
+canvas = Canvas(
+    window,
+    bg = "#ffffff",
+    height = 600,
+    width = 1000,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
+
+background_img = PhotoImage(file = f"background.png")
+background = canvas.create_image(
+    508.0, 309.0,
+    image=background_img)
+
+img0 = PhotoImage(file = f"img0.png")
+b0 = Button(
+    image = img0,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b0.place(
+    x = 165, y = 208,
+    width = 130,
+    height = 133)
+
+img1 = PhotoImage(file = f"img1.png")
+b1 = Button(
+    image = img1,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b1.place(
+    x = 165, y = 376,
+    width = 130,
+    height = 143)
+
+img2 = PhotoImage(file = f"img2.png")
+b2 = Button(
+    image = img2,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b2.place(
+    x = 860, y = 33,
+    width = 117,
+    height = 37)
+
+window.resizable(False, False)
+window.mainloop()
